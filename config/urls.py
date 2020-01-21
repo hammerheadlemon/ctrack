7a65d7e12c30992ceb6b94acc55d2ee152687b6e
Matthew Lemon <lemon@matthewlemon.com>
2020-01-21 19:23:34 +0000

    changed admin site header - very easily: thanks

--- config/urls.py
+++ config/urls.py
@@ -5,6 +5,9 @@ from django.contrib import admin
 from django.views.generic import TemplateView
 from django.views import defaults as default_views
 
+# change title
+admin.site.site_header = "ctrack admin"
+
 urlpatterns = [
     path("", TemplateView.as_view(template_name="pages/home.html"), name="home"),
     path(
