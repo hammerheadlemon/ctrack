00b2dd58f3e58f0d55acd3d6b36ab9de85855a25
Matthew Lemon <matt@matthewlemon.com>
2020-03-04 21:27:08 +0000

    improved admin list for CAFs now

--- ctrack/caf/admin.py
+++ ctrack/caf/admin.py
@@ -14,12 +14,17 @@ class ApplicableSystemAdmin(admin.StackedInline):
     extra = 1
 
 
+def get_caf_name(obj):
+    ass = ApplicableSystem.objects.filter(caf=obj).first()
+    return f"{ass.organisation.name}_v{obj.version}"
+
+
 class CAFAdmin(admin.ModelAdmin):
     # TODO - we need the CAF list to show essential services
     #  but this is a many-to-many relationship, so we need to summarise it somehow
     model = CAF
     inlines = [ApplicableSystemAdmin]
-    list_display = ["quality_grading", "confidence_grading", "file"]
+    list_display = [get_caf_name, "quality_grading", "confidence_grading", "file"]
 
 
 admin.site.register(CAF, CAFAdmin)
