e9c5aacdfebe65cc865cb9c2b56f456daa1214c1
Matthew Lemon <lemon@matthewlemon.com>
2020-05-23 16:27:07 +0100

    removed unneeded imports

--- ctrack/users/tests/test_models.py
+++ ctrack/users/tests/test_models.py
@@ -1,12 +1,5 @@
 import pytest
-from django.contrib.auth import get_user_model
 
-from ctrack.organisations.models import Mode, Person, Submode
-from ctrack.organisations.tests.factories import (
-    OrganisationFactory,
-    PersonFactory,
-    RoleFactory,
-)
 from ctrack.users.models import User
 
 pytestmark = pytest.mark.django_db
@@ -17,6 +10,7 @@ def test_user_get_absolute_url(user: User):
 
 
 def test_user_is_person_object(user: User):
+
     """User comes from ctrack.conftest.
     """
     assert user.oes_user is False
