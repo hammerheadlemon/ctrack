2fe19119ae271e0a6dc94fdf37c646d7bfb8e767
Matthew Lemon <lemon@matthewlemon.com>
2020-10-15 08:49:37 +0100

    new bootstrap static files, new datatables static files

--- ctrack/static/js/project.js
+++ ctrack/static/js/project.js
@@ -1,9 +1,1 @@
 /* Project specific Javascript goes here. */
-$(document).ready(function() {
-    $('#datatable').DataTable({
-        ordering: true,
-        searching: true,
-        dom: 'B<"clear">lfrtip',
-        buttons: true
-    });
-} );
